050f70a679d44c0c68e75ff256013dcf9b618e79
BrunoMaiolo <brunosj@hotmail.it>
2026-01-08 15:29:14 +0100

    funzione

--- main2.js
+++ main2.js
@@ -1,3 +1,7 @@
 let userChoice = prompt("Choose even or odd")
 
 let userNumber = Number(prompt("Choose a number from 1 to 5"));
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 5) + 1;
+}
