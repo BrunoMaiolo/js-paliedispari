73473fce8c5c52d16d9f69f969f159c9d3025d53
BrunoMaiolo <brunosj@hotmail.it>
2026-01-08 15:32:19 +0100

    usare la funzione e sommare i numeri

--- main2.js
+++ main2.js
@@ -5,3 +5,7 @@ let userNumber = Number(prompt("Choose a number from 1 to 5"));
 function getRandomNumber() {
   return Math.floor(Math.random() * 5) + 1;
 }
+
+let computerNumber = getRandomNumber();
+let sum = userNumber + computerNumber;
+
